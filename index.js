b49aaf088c89dd185e26eb77a78dbe2e4e20876f
FrizzyBlues <25872525+FrizzyBlues@users.noreply.github.com>
2021-12-28 23:59:28 -0500

    testing with programming languages api

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const basePath = process.cwd();
 const sha1 = require(`${basePath}/node_modules/sha1`);
 const { startCreating, buildSetup, getElements, getRandomLayers, createRandomArt, createDna, buildSetupV2, isNewDnaUnique, addToExistingDnaList } = require(`${basePath}/src/main.js`);
 const { uploadMedia, uploadMetadata } = require(`${basePath}/src/upload.js`);
-
-// const express = require('express');
-// const swaggerJSDoc = require('swagger-jsdoc');
-// const swaggerUI = require('swagger-ui-express');
-
-
-// (() => {
-//   buildSetup();
-//   startCreating();
-// })();
+const express = require("express");
+const fs = require('fs')
+const stream = require('stream');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 const cleanup = () => {
   buildSetup();
@@ -26,34 +22,25 @@ const generateOne = () => {
   return newDna;
 }
 
-// const createReadStream = () => {
-//   let r = null;
-//   try {
-//     r = fs.createReadStream('./build/images/0.png') // or any other way to get a readable stream
-//   } catch (e) {
-//     console.log('too soon to view');
-//   }
-//   return r;
-// }
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-const express = require("express");
-const fs = require('fs')
-const stream = require('stream');
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("The Adorable");
-});
+app.get('/', (req, res) => {
+  res.json({'message': 'ok'});
+})
 
 app.get("/generate", (req, res) => {
     const dna = generateOne();
-    res.send(`${dna}`);
+    res.json({"dna": `${dna}`});
 });
 
 app.get("/cleanup", (req, res) => {
   cleanup();
-  res.send("Cleanup successful");
+  res.json({"message": "Cleanup successful"});
 });
 
 app.get('/image/:uniqueDna',(req, res) => {
@@ -78,12 +65,22 @@ app.get('/save/:uniqueDna',(req, res) => {
     uploadMedia(uniqueDna, `./build/images/${uniqueDna}.png`);
     uploadMetadata(uniqueDna, `./build/json/${uniqueDna}.json`);
     addToExistingDnaList(uniqueDna);
-    res.send(`Saved! New DNA ${uniqueDna}`);
+    res.json({"message": `Saved! New DNA ${uniqueDna}`});
   } else {
-    res.send(`Failed! Existing DNA ${uniqueDna}`);
+    res.json({"message": `Failed! Existing DNA ${uniqueDna}`});
   }
   
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Server listening at port 3000'));
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
