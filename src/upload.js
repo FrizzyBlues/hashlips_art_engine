e6628b695d6ed90d7fd2ccd75ddcadd91a489f2a
FrizzyBlues <25872525+FrizzyBlues@users.noreply.github.com>
2021-12-28 22:57:40 -0500

    update system reserved env variables

--- src/upload.js
+++ src/upload.js
@@ -5,14 +5,14 @@ const fs = require("fs");
 const s3 = new AWS.S3({
     apiVersion: "2010-12-01",
     region: "us-east-1",
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_FZ,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_FZ, 
 });
 
 const uploadMedia = (fileName, fileLocation) => {
     const fileContent = fs.readFileSync(fileLocation);
     const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
+        Bucket: process.env.AWS_BUCKET_NAME_FZ,
         Key: `art/devnet/media/${fileName}.png`, 
         Body: fileContent
     };
@@ -24,7 +24,7 @@ const uploadMedia = (fileName, fileLocation) => {
 const uploadMetadata = (fileName, fileLocation) => {
     const fileContent = fs.readFileSync(fileLocation);
     const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
+        Bucket: process.env.AWS_BUCKET_NAME_FZ,
         Key: `art/devnet/metadata/${fileName}.json`, 
         Body: fileContent
     };
